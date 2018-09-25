5b03b5aff10590cb5e502f6e8559aff88e9fa638
luozhen <1783921704@qq.com>
2018-09-25 15:10:16 +0800

    将MyBlockingLinkedList改为无界队列

--- src/thread/collection/MyBlockingLinkedList.java
+++ src/thread/collection/MyBlockingLinkedList.java
@@ -7,7 +7,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Class MyBlockingLinkedList
- * @Description: TODO
+ * @Description: 自定义无界BlockingQueue
  * @Author: luozhen
  * @Create: 2018/09/25 14:10
  */
@@ -23,28 +23,28 @@ public class MyBlockingLinkedList<T> {
     private final int minSize = 0;
 
     // 队列的最大size
-    private final int maxSize;
+//    private final int maxSize;
 
-    public MyBlockingLinkedList(int maxSize) {
-        this.maxSize = maxSize;
-    }
+//    public MyBlockingLinkedList(int maxSize) {
+//        this.maxSize = maxSize;
+//    }
 
     private final Object lock = new Object();
 
     public void put(T t) {
-        synchronized (lock) {
-            while (count.get() == this.maxSize) {
-                try {
-                    lock.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+//        synchronized (lock) {
+//            while (count.get() == this.maxSize) {
+//                try {
+//                    lock.wait();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
             linkedList.add(t);
             count.incrementAndGet();
             lock.notify();
             //System.out.println("新加入的元素为：" + t);
-        }
+//        }
     }
 
     public T take() {
@@ -69,37 +69,38 @@ public class MyBlockingLinkedList<T> {
     }
 
     public static void main(String[] args) {
-        MyBlockingLinkedList<Character> linkedList = new MyBlockingLinkedList<>(5);
+//        MyBlockingLinkedList<Character> linkedList = new MyBlockingLinkedList<>(5);
+        MyBlockingLinkedList<Character> linkedList = new MyBlockingLinkedList<>();
 
         ExecutorService executorService = Executors.newCachedThreadPool();
 
-        executorService.execute(() -> {
-            Character temp = 'a';
-            for (int i = 0; i < 10; i++) {
-                linkedList.put(temp);
-                System.out.println("producer produce : " + temp);
-                temp++;
-                System.out.println("--------------------------------");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        executorService.execute(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("队列长度为 : " + linkedList.getSize());
-                Character c = linkedList.take();
-                System.out.println("consumer consume : " + c);
-                System.out.println("队列长度为 : " + linkedList.getSize());
-            }
-        });
+//        executorService.execute(() -> {
+//            Character temp = 'a';
+//            for (int i = 0; i < 10; i++) {
+//                linkedList.put(temp);
+//                System.out.println("producer produce : " + temp);
+//                temp++;
+//                System.out.println("--------------------------------");
+//                try {
+//                    Thread.sleep(100);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//        executorService.execute(() -> {
+//            for (int i = 0; i < 10; i++) {
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("队列长度为 : " + linkedList.getSize());
+//                Character c = linkedList.take();
+//                System.out.println("consumer consume : " + c);
+//                System.out.println("队列长度为 : " + linkedList.getSize());
+//            }
+//        });
 
     }
 
